e68a45d4b8b10cf51aa797accb6441cede4d86f5
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-04-27 14:23:20 +0530

    Add files via upload

---
+++ Batch4119hibernateonetowone/src/main/java/com/sana/onetwoone/MainApp.java
@@ -0,0 +1,18 @@
+package com.sana.onetwoone;
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+public class MainApp {
+public static void main(String[] args) {
+Person deepak = new Person("deepak kumar","Delhi");
+Adhaar ada = new Adhaar(71122345);
+deepak.setAdhar(ada);
+Transaction txn;
+try(Session session=Hibernateutil.getSessionFactory().openSession()){
+txn = session.beginTransaction();
+session.save(deepak);
+txn.commit();
+}
+}
+}
